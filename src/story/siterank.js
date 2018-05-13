2e3ba513cd9a793e95f244affe2ca785768c0e13
NeonWilderness <neonwilderness@ymail.com>
2018-05-13 17:27:45 +0200

    Fix KO select options issue

--- src/story/siterank.js
+++ src/story/siterank.js
@@ -3,6 +3,14 @@
   $.getJSON('https://rawgit.com/NeonWilderness/tdhttp/master/Twoday_SiteIdRank.json', function (json) {
     $(function () {
       var ViewModel = function () {
+        this.activeOptions = [
+          { name: 'Einen Monat (30 Tage)', val: '30'},
+          { name: 'Zwei Monate (60 Tage)', val: '60'},
+          { name: 'Drei Monate (90 Tage)', val: '90'},
+          { name: 'Vier Monate (120 Tage)', val: '120'},
+          { name: 'Fünf Monate (150 Tage)', val: '150'},
+          { name: 'Sechs Monate (180 Tage)', val: '180'}
+        ];
         this.activeTime = ko.observable('180');
         this.getRank = function () {
           var activeTimeSpan = parseInt(this.activeTime());
